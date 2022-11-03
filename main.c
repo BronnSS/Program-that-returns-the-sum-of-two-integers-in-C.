def6770f6a1514275276179e859cde04c944118d
İbrahim BİNER <117264885+BronnSS@users.noreply.github.com>
2022-11-03 09:47:38 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <conio.h>
+
+
+
+
+
+
+
+int main()
+{
+    int sayi1,sayi2,toplam;
+    printf("Birici sayi :");
+    scanf("%d",&sayi1);
+    printf("ikinci sayi :");
+    scanf("%d",&sayi2);
+
+    toplam=sayi1+sayi2;
+
+    printf("\nToplam: %d",toplam);
+
+    return 0;
+}
+
